d8f5620d86ba995c93d2e9a309669452e5c9f494
shuren <1377352087@qq.com>
2021-12-26 15:13:26 +0800

    第一次提交

---
+++ main_cpu_5/main_cpu_5.srcs/sources_1/new/defines.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2021/12/26 15:12:13
+// Design Name: 
+// Module Name: defines
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module defines(
+
+    );
+endmodule
